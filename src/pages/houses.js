dfc917fbe3018aede63f3b43f0f7cfe6699d7d3a
aladdin4u <taofikakinkumi@gmail.com>
2024-01-02 04:21:55 +0100

    added react-icons

--- src/pages/houses.js
+++ src/pages/houses.js
@@ -32,6 +32,8 @@ import {
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { FaBath, FaCar, FaBed } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
 const houses = [
   {
     id: 1,
@@ -170,7 +172,7 @@ export default function House({ className, ...props }) {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <Card className={cn("w-[380px]", className)} {...props}>
+      <Card className={cn("w-md", className)} {...props}>
         <CardHeader>
           <CardTitle>Real Estate Price</CardTitle>
           <CardDescription>
@@ -178,147 +180,186 @@ export default function House({ className, ...props }) {
           </CardDescription>
         </CardHeader>
         <CardContent className="grid gap-4">
-          <div className=" flex items-center space-x-4 rounded-md border p-4">
+          <div className=" flex items-center space-x-4 border-y p-4">
             <Form {...form}>
               <form
                 onSubmit={form.handleSubmit(onSubmit)}
                 className="space-y-8"
               >
-                <FormField
-                  control={form.control}
-                  rules={{ required: "This field is required", min: 1 }}
-                  name="bedrooms"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Bed Rooms</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="number"
-                          placeholder="bedsrooms"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormDescription>
-                        Total Bed Rooms within a block.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  rules={{ required: "This field is required", min: 1 }}
-                  name="toilets"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Toilets</FormLabel>
-                      <FormControl>
-                        <Input type="number" placeholder="toilets" {...field} />
-                      </FormControl>
-                      <FormDescription>
-                        Total Toilets within a block.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  rules={{ required: "This field is required", min: 1 }}
-                  name="parking_space"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Garage</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="number"
-                          placeholder="Parking Space"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormDescription>
-                        Total Parking Space within a block.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  rules={{ required: "This field is required" }}
-                  name="town"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Town</FormLabel>
-                      <FormControl>
-                        <Input placeholder="town" {...field} />
-                      </FormControl>
-                      <FormDescription>
-                        House Local Goverment Address.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="state"
-                  rules={{ required: "This field is required" }}
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>State</FormLabel>
-                      <FormControl>
-                        <Input placeholder="State" {...field} />
-                      </FormControl>
-                      <FormDescription>House State Address.</FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit">Submit</Button>
+                <div className="grid grid-cols-2 gap-4">
+                  <FormField
+                    control={form.control}
+                    rules={{
+                      required: "This field is required",
+                      min: {
+                        value: 1,
+                        message: "This input is below minimum value",
+                      },
+                    }}
+                    name="bedrooms"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Bed Rooms</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="number"
+                            placeholder="bedsrooms"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormDescription>
+                          Total Bed Rooms within a block.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    rules={{
+                      required: "This field is required",
+                      min: {
+                        value: 1,
+                        message: "This input is below minimum value",
+                      },
+                    }}
+                    name="toilets"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Toilets</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="number"
+                            placeholder="toilets"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormDescription>
+                          Total Toilets within a block.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="grid grid-cols-2 gap-4">
+                  <FormField
+                    control={form.control}
+                    rules={{
+                      required: "This field is required",
+                      min: {
+                        value: 1,
+                        message: "This input is below minimum value",
+                      },
+                    }}
+                    name="parking_space"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Garage</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="number"
+                            placeholder="Parking Space"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormDescription>
+                          Total Parking Space within a block.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="space-y-2">
+                  <h2>Address</h2>
+                  <div className="grid grid-cols-2 gap-4">
+                    <FormField
+                      control={form.control}
+                      rules={{ required: "This field is required" }}
+                      name="town"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Town</FormLabel>
+                          <FormControl>
+                            <Input placeholder="town" {...field} />
+                          </FormControl>
+                          <FormDescription>
+                            House Local Goverment Address.
+                          </FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="state"
+                      rules={{ required: "This field is required" }}
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>State</FormLabel>
+                          <FormControl>
+                            <Input placeholder="State" {...field} />
+                          </FormControl>
+                          <FormDescription>
+                            House State Address.
+                          </FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </div>
+                <Button type="submit" className="w-full">
+                  Predict
+                </Button>
               </form>
             </Form>
           </div>
         </CardContent>
         <CardFooter>
-          <Button className="w-full">Mark all as read</Button>
+          &copy; Real Estate Forcast
         </CardFooter>
       </Card>
-      <h2>Browse houses</h2>
-      <div className="mb-32 grid text-center gap-4 lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
+      <h2 className="my-4 self-start text-2xl font-medium">Best for you</h2>
+      <div className="mb-32 grid text-center gap-4 lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
         {currentItems.map((r, index) => {
           return (
             <Card key={index} className="w-auto">
               <CardHeader>
-                <CardTitle>{r.title}</CardTitle>
-                <CardDescription>
-                  Lorem ipsum {r.bathrooms} dolor sit amet consectetur
-                  adipisicing elit. Ipsam quae consequuntur optio ratione
-                  corrupti hic sapiente, unde non nisi magni quaerat magnam iste
-                  rem est, in qui quas minima itaque.
+                <CardTitle className="text-left">{r.title}</CardTitle>
+                <CardDescription className="text-left space-y-2">
+                  <span className="flex items-center gap-2">
+                    <FaLocationDot />
+                    {r.town}, {r.state}
+                  </span>
+                  <span className="block flex space-x-4">
+                    <span className="flex gap-2 text-sm font-medium leading-none">
+                      <FaBed />
+                      {r.bedrooms} Beds
+                    </span>
+                    <span className="flex gap-2 text-sm font-medium leading-none">
+                      <FaBath />
+                      {r.toilets} Baths
+                    </span>
+                    <span className="flex gap-2 text-sm font-medium leading-none">
+                      <FaCar />
+                      {r.parking_space} Garage
+                    </span>
+                  </span>
                 </CardDescription>
               </CardHeader>
-              <CardContent>
-                <p>
-                  {r.town}, {r.state}
-                </p>
-                <div className="space-x-4">
-                  <span className="text-sm font-medium leading-none">
-                    {r.bedrooms} Beds
-                  </span>
-                  <span className="text-sm font-medium leading-none">
-                    {r.toilets} Baths
-                  </span>
-                  <span className="text-sm font-medium leading-none">
-                    {r.parking_space} Garage
-                  </span>
-                </div>
+              <CardContent className="text-justify">
+                Lorem ipsum {r.bathrooms} dolor sit amet consectetur adipisicing
+                elit. Ipsam quae consequuntur optio ratione corrupti hic
+                sapiente, unde non nisi magni quaerat magnam iste rem est, in
+                qui quas minima itaque.
               </CardContent>
               <CardFooter>
                 <div className="text-left">
-                  <p className="text-sm">price</p>
+                  <span className="text-sm">price</span>
                   <p className="text-sm font-bold leading-none">
-                    N{r.price}/mo
+                    &#8358;{r.price}/mo
                   </p>
                 </div>
               </CardFooter>
